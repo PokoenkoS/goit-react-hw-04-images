3748233b376b280ecb8a9bc2d521123a3e2b5ee9
PokoenkoS <pokoenkosveta@gmail.com>
2023-03-23 20:45:41 +0200

    add

--- src/components/ImageApi.js
+++ src/components/ImageApi.js
@@ -1,7 +1,8 @@
 
 function fetchImages(formValue, page){
     const API_KEY = '33261865-905999929b5f445e8a29b592f';
-    return fetch(`https://pixabay.com/api/?q=${formValue}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`)
+    const URL = `https://pixabay.com/api/`
+    return fetch(`${URL}?q=${formValue}&page=${page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`)
     .then(response => {
         if (response.ok) {
           return response.json();
--- src/components/ImageGalleryItem.js
+++ src/components/ImageGalleryItem.js
@@ -1,34 +1,27 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import  "./styles.css";
 import Modal from "./Modal";
 
-   class ImageGallaryItem extends Component  {
-
-      state={
-         isSelect:'',
-      }
-
-onToggle=()=>{
-   this.setState(({ isSelect }) => ({ isSelect: !isSelect }));
-}
-      render() {
-         const { isSelect } = this.state;
-         const {item}=this.props
-         return( <>
-            
-   <li className="ImageGalleryItem" key={item.id} >
-      <img src={item.webformatURL} 
-      alt={item.tags}
-       className="ImageGalleryItem-image"
-       onClick={this.onToggle}/>
-   </li>
-   {isSelect && <Modal onClose={this.onToggle} item={item}/>}
-     </>
-    )
-      }
-       
-   
-          
-        }
+   const ImageGallaryItem =({item})=> {
+     const [isSelect, setIsSelect] = useState('');
+    
+     const onToggle=()=>{
+   setIsSelect((prev) => (!isSelect));
+  }
+ 
+        return (
+         <>
+         <li className="ImageGalleryItem" key={item.id} >
+         <img src={item.webformatURL} 
+         alt={item.tags}
+          className="ImageGalleryItem-image"
+          onClick={onToggle}/>
+      </li>
+      isSelect && (<Modal onClose={onToggle} item={item}/>)
+      </>
+  )}
+      
   
+      
+ 
 export default ImageGallaryItem;
